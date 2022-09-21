1faf2830bc7cb70a8adb871ba9fb4ad2eae861ad
Gary Wong <gary.wong.fiji@gmail.com>
2022-09-21 11:00:14 +1000

    Singleton class

---
+++ TechnicalAssessment_Gary/CountryFinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalAssessment_Gary
+{
+    internal class CountryFinder
+    {
+        private static CountryFinder instance = null;
+
+        private  CountryFinder()
+        {
+
+        }
+
+        public static CountryFinder getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new CountryFinder(); 
+            }
+
+            return instance;
+        }
+    }
+}
